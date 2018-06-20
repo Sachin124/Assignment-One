cfd2da633538f25bc5e040c4b1e23858593426dc
sachinb124 <sachinb2193>
2018-06-21 00:42:16 +0530

    Third Assignment

---
+++ assignmentOne/src/app/second-component/second-component.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-second-component',
+  templateUrl: './second-component.component.html',
+  styleUrls: ['./second-component.component.css']
+})
+export class SecondComponentComponent implements OnInit {
+
+  componentName = 'Inside Second Component';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
